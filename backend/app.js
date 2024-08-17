c7eff6a6da1d15582ce8de7d7f06a6f285023668
Justin <fernbaughj@gmail.com>
2024-08-17 09:24:13 -0700

    Allow cors from anywhere to API routes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 import express from "express";
 import appointments from "./routes/appointments.js?";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+	cors({
+		origin: "*",
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+		preflightContinue: false,
+		optionsSuccessStatus: 204,
+	})
+);
+
 // get port from environment variable or use default port 8000
 const port = process.env.PORT || 8000;
 
